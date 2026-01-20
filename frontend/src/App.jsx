4802331ae3598c068c96f4bc3e9529f3f257c796
DinoRamielson34 <ramielsoadino@gmail.com>
2026-01-20 11:15:55 +0300

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import Authentification from './crud-authentification/user-list.jsx'
 
 export default function App(){
   return (
     <div className="app">
       <h1>Projet Cloud S5</h1>
-      <p>Frontend React minimal (Vite).</p>
+      <p>Frontend React  (Vite).</p>
+
+      <Authentification />
     </div>
   )
 }
